baf1f7b091d747a5d15659a795e7eca5a247cd8c
amitcha06 <amitchayko@gmail.com>
2024-01-17 21:36:47 +0200

    created the new SpecificVars

---
+++ lib/views/mobile/screens/specific_view/specific_vars.dart
@@ -0,0 +1,132 @@
+import "package:flutter/material.dart";
+import "package:orbit_standard_library/orbit_standard_library.dart";
+import "package:scouting_frontend/models/id_providers.dart";
+import "package:scouting_frontend/models/matches_model.dart";
+import "package:scouting_frontend/models/team_model.dart";
+import "package:scouting_frontend/views/mobile/hasura_vars.dart";
+
+class SpecificVars implements HasuraVars {
+  SpecificVars(final BuildContext context)
+      : team = null,
+        driveRating = null,
+        intakeRating = null,
+        speakerRating = null,
+        ampRating = null,
+        defenseRating = null,
+        climbRating = null,
+        generalRating = null,
+        scheduleMatch = null,
+        name = "",
+        isRematch = false,
+        defenseAmount = 1,
+        faultMessage = null,
+        autoPathUrl = "";
+
+  SpecificVars.all({
+    required this.team,
+    required this.driveRating,
+    required this.intakeRating,
+    required this.speakerRating,
+    required this.ampRating,
+    required this.defenseRating,
+    required this.climbRating,
+    required this.generalRating,
+    required this.scheduleMatch,
+    required this.name,
+    required this.isRematch,
+    required this.defenseAmount,
+    required this.faultMessage,
+    required this.autoPathUrl,
+  });
+
+  SpecificVars copyWith({
+    final LightTeam? Function()? team,
+    final int? Function()? driveRating,
+    final int? Function()? intakeRating,
+    final int? Function()? speakerRating,
+    final int? Function()? ampRating,
+    final int? Function()? defenseRating,
+    final int? Function()? climbRating,
+    final int? Function()? generalRating,
+    final ScheduleMatch? Function()? scheduleMatch,
+    final String Function()? name,
+    final bool Function()? isRematch,
+    final int Function()? defenseAmount,
+    final String? Function()? faultMessage,
+    final String Function()? autoPathUrl,
+  }) =>
+      SpecificVars.all(
+        team: team != null ? team() : this.team,
+        driveRating: driveRating != null ? driveRating() : this.driveRating,
+        intakeRating: intakeRating != null ? intakeRating() : this.intakeRating,
+        speakerRating:
+            speakerRating != null ? speakerRating() : this.speakerRating,
+        ampRating: ampRating != null ? ampRating() : this.ampRating,
+        defenseRating:
+            defenseRating != null ? defenseRating() : this.defenseRating,
+        climbRating: climbRating != null ? climbRating() : this.climbRating,
+        generalRating:
+            generalRating != null ? generalRating() : this.generalRating,
+        scheduleMatch:
+            scheduleMatch != null ? scheduleMatch() : this.scheduleMatch,
+        name: name != null ? name() : this.name,
+        isRematch: isRematch != null ? isRematch() : this.isRematch,
+        defenseAmount:
+            defenseAmount != null ? defenseAmount() : this.defenseAmount,
+        faultMessage: faultMessage != null ? faultMessage() : this.faultMessage,
+        autoPathUrl: autoPathUrl != null ? autoPathUrl() : this.autoPathUrl,
+      );
+
+  final LightTeam? team;
+  final int? driveRating;
+  final int? intakeRating;
+  final int? speakerRating;
+  final int? ampRating;
+  final int? defenseRating;
+  final int? climbRating;
+  final int? generalRating;
+  final ScheduleMatch? scheduleMatch;
+  final String name;
+  final bool isRematch;
+  final int defenseAmount;
+  final String? faultMessage;
+  final String autoPathUrl;
+  @override
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        "team_id": team?.id,
+        "path_url": autoPathUrl,
+        "driving_rating": driveRating,
+        "intake_rating": intakeRating,
+        "climb_rating": climbRating,
+        "amp_rating": ampRating,
+        "speaker_rating": speakerRating,
+        "defense_rating": defenseRating,
+        "general_rating": generalRating,
+        "is_rematch": isRematch,
+        "schedule_match_id": scheduleMatch?.id,
+        "scouter_name": name,
+        "defense_amount_id": defenseAmount,
+        if (faultMessage != null) "match_number": scheduleMatch?.matchNumber,
+        if (faultMessage != null) "fault_message": faultMessage,
+        if (faultMessage != null) "match_type_id": scheduleMatch?.matchTypeId,
+      };
+
+  void reset(final BuildContext context) {
+    copyWith(
+      isRematch: always(false),
+      scheduleMatch: always(null),
+      faultMessage: always(null),
+      team: always(null),
+      driveRating: always(null),
+      climbRating: always(null),
+      intakeRating: always(null),
+      ampRating: always(null),
+      speakerRating: always(null),
+      defenseRating: always(null),
+      generalRating: always(null),
+      autoPathUrl: always(""),
+      defenseAmount:
+          always(IdProvider.of(context).defense.nameToId["No Defense"]!),
+    );
+  }
+}
