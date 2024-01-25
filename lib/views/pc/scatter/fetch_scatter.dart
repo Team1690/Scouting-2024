c025657aefee8c9b66620043b6943a1b3a33e219
unknown <orikrisi90@gmail.com>
2024-01-25 17:17:00 +0200

    fetch_scatter_fix

--- lib/views/pc/scatter/fetch_scatter.dart
+++ lib/views/pc/scatter/fetch_scatter.dart
@@ -15,63 +15,42 @@ query Scatter {
     technical_matches_aggregate(where: {ignored: {_eq: false}}) {
     aggregate {
       stddev{
-        auto_cones_low
-        auto_cones_mid
-        auto_cones_top
-        auto_cubes_low
-        auto_cubes_mid
-        auto_cubes_top
-        tele_cones_low
-        tele_cones_mid
-        tele_cones_top
-        tele_cubes_low
-        tele_cubes_mid
-        tele_cubes_top
-        auto_cones_delivered
-        auto_cubes_delivered
-        tele_cones_delivered
-        tele_cubes_delivered
+			auto_amp
+      auto_amp_missed
+      tele_amp
+      tele_amp_missed
+      auto_speaker
+      auto_speaker_missed
+      tele_speaker
+      tele_speaker_missed
       }
       avg {
-        auto_cones_low
-        auto_cones_mid
-        auto_cones_top
-        auto_cubes_low
-        auto_cubes_mid
-        auto_cubes_top
-        tele_cones_low
-        tele_cones_mid
-        tele_cones_top
-        tele_cubes_low
-        tele_cubes_mid
-        tele_cubes_top
-        auto_cones_delivered
-        auto_cubes_delivered
-        tele_cones_delivered
-        tele_cubes_delivered
+			auto_amp
+      auto_amp_missed
+      tele_amp
+      tele_amp_missed
+      auto_speaker
+      auto_speaker_missed
+      tele_speaker
+      tele_speaker_missed
+    
       }
     }
   }
     technical_matches(where: {ignored: {_eq: false}}) {
-    auto_cones_low
-    auto_cones_mid
-    auto_cones_top
-    auto_cubes_low
-    auto_cubes_mid
-    auto_cubes_top
-    tele_cones_low
-    tele_cones_mid
-    tele_cones_top
-    tele_cubes_low
-    tele_cubes_mid
-    tele_cubes_top
-    auto_cones_delivered
-    auto_cubes_delivered
-    tele_cones_delivered
-    tele_cubes_delivered
+   		auto_amp
+      auto_amp_missed
+      tele_amp
+      tele_amp_missed
+      auto_speaker
+      auto_speaker_missed
+      tele_speaker
+      tele_speaker_missed
+    
   }
   }
 }
+
 """;
 Future<List<ScatterData>> fetchScatterData() async {
   final GraphQLClient client = getClient();
@@ -82,12 +61,7 @@ Future<List<ScatterData>> fetchScatterData() async {
       parserFn: (final Map<String, dynamic> data) =>
           (data["team"] as List<dynamic>)
               .map<ScatterData?>((final dynamic scatterTeam) {
-                final LightTeam team = LightTeam(
-                  scatterTeam["id"] as int,
-                  scatterTeam["number"] as int,
-                  scatterTeam["name"] as String,
-                  scatterTeam["colors_index"] as int,
-                );
+                final LightTeam team = LightTeam.fromJson(scatterTeam);
                 final dynamic avg = scatterTeam["technical_matches_aggregate"]
                     ["aggregate"]["avg"];
                 final dynamic stddev =
@@ -95,7 +69,8 @@ Future<List<ScatterData>> fetchScatterData() async {
                         ["stddev"];
                 final List<dynamic> matches =
                     scatterTeam["technical_matches"] as List<dynamic>;
-                if (avg["auto_cones_top"] == null) {
+                // error in this line is returning null put the right value that is the avrage of the gamepiece
+                if (avg["avg"] == null) {
                   //if one of these is null, the team's match data doesnt exist so we return null
                   return null;
                 }
@@ -108,7 +83,7 @@ Future<List<ScatterData>> fetchScatterData() async {
                           (matchPoints - avgGamepiecePoints).abs(),
                     )
                     .average;
-                final double gamepiecesStddev = stddev["auto_cones_top"] == null
+                final double gamepiecesStddev = stddev["auto_speaker"] == null
                     ? 0
                     : getPieces(parseMatch(stddev));
                 final double avgGamepieces = getPieces(parseMatch(avg));
@@ -120,7 +95,7 @@ Future<List<ScatterData>> fetchScatterData() async {
                   gamepiecesStddev,
                 );
               })
-              .whereType<ScatterData>()
+              .whereNotNull()
               .toList(),
     ),
   );
