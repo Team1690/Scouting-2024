75c1d75cd61b164c653e67fc68ca13e6caf23d84
IdoDePro <idozipori08@gmail.com>
2024-01-31 18:30:59 +0200

    removed query test:(

--- test/hasura_helper_test.dart
+++ test/hasura_helper_test.dart
@@ -1,14 +1,7 @@
 import "package:flutter_test/flutter_test.dart";
-
 import "package:orbit_standard_library/orbit_standard_library.dart";
-import "package:scouting_frontend/views/common/fetch_functions/single-multiple_teams/fetch_single_team.dart";
 
 void main() {
-  group("Query", () {
-    test("Team Query", () {
-      expect(fetchSingleTeamData(12), completes);
-    });
-  });
   group("Map Extenstions", () {
     group("Map Nullable", () {
       test("Actually Null", () {
