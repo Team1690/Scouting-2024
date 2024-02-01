597fbc88006de6be7781b5b92c39555ad13f99c7
amitaskof <amit.askof@gmail.com>
2024-02-01 16:48:26 +0200

    fix lint error

--- lib/views/common/fetch_functions/technical_match_data.dart
+++ lib/views/common/fetch_functions/technical_match_data.dart
@@ -1,5 +1,4 @@
 import "package:scouting_frontend/models/helpers.dart";
-import "package:scouting_frontend/views/common/fetch_functions/all_teams/fetch_all_teams.dart";
 import "package:scouting_frontend/views/common/fetch_functions/climb_enum.dart";
 import "package:scouting_frontend/views/common/fetch_functions/parse_match_functions.dart";
 import "package:scouting_frontend/views/pc/team_info/models/team_info_classes.dart";
