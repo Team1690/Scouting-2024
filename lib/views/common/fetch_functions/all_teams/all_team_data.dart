7a1a542bdddd3f4ab2ddc598f91ac5c3602bf6f5
unknown <orikrisi90@gmail.com>
2024-02-01 21:01:54 +0200

    scatter fi x

--- lib/views/common/fetch_functions/all_teams/all_team_data.dart
+++ lib/views/common/fetch_functions/all_teams/all_team_data.dart
@@ -1,21 +1,22 @@
 import "package:scouting_frontend/models/team_model.dart";
 
 class AllTeamData {
-  AllTeamData(
-      {required this.team,
-      required this.firstPicklistIndex,
-      required this.secondPicklistIndex,
-      required this.autoGamepieceAvg,
-      required this.teleGamepieceAvg,
-      required this.gamepieceAvg,
-      required this.missedAvg,
-      required this.gamepiecePointAvg,
-      required this.brokenMatches,
-      required this.amountOfMatches,
-      required this.matchesClimbed,
-      required this.trapAverage,
-      required this.yStddevGamepiecePoints,
-      required this.gamepiecesStddev});
+  AllTeamData({
+    required this.team,
+    required this.firstPicklistIndex,
+    required this.secondPicklistIndex,
+    required this.autoGamepieceAvg,
+    required this.teleGamepieceAvg,
+    required this.gamepieceAvg,
+    required this.missedAvg,
+    required this.gamepiecePointAvg,
+    required this.brokenMatches,
+    required this.amountOfMatches,
+    required this.matchesClimbed,
+    required this.trapAverage,
+    required this.yStddevGamepiecePoints,
+    required this.gamepiecesStddev,
+  });
   final double gamepiecesStddev;
   final double yStddevGamepiecePoints;
   final LightTeam team;
