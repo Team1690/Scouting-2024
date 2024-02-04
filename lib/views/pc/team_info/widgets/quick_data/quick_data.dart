04742636bef75a926f8f88d70f764560b79f0828
amitcha06 <amitchayko@gmail.com>
2024-02-04 11:25:49 +0200

    quickdata UI

--- lib/views/pc/team_info/widgets/quick_data/quick_data.dart
+++ lib/views/pc/team_info/widgets/quick_data/quick_data.dart
@@ -16,58 +16,104 @@ class QuickDataCard extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   children: <Widget>[
-                    const Column(
+                    Column(
                       children: <Widget>[
-                        Text(
-                          "Auto",
+                        const Text(
+                          "Amp",
                           style: TextStyle(fontSize: 18),
                         ),
-                        //TODO Auto quickdata
+                        Text(
+                            "Avg Auto Amp: ${data.autoAmpAvg.toStringAsFixed(1)}"),
+                        Text(
+                            "Avg Tele Amp: ${data.teleAmpAvg.toStringAsFixed(1)}"),
+                        Text(
+                            "Avg Total Amp: ${(data.teleAmpAvg + data.autoAmpAvg).toStringAsFixed(1)}"),
+                        Text(
+                            "Max Total Amp: ${(data.bestAmpGamepiecesSum).toStringAsFixed(1)}"),
                       ],
                     ),
-                    const Column(
+                    Column(
                       children: <Widget>[
-                        Text(
-                          "Teleop",
+                        const Text(
+                          "Amp",
                           style: TextStyle(fontSize: 18),
                         ),
-                        //TODO tele quickdata
+                        Text(
+                            "Avg Auto Speaker: ${data.autoSpeakerAvg.toStringAsFixed(1)}"),
+                        Text(
+                            "Avg Tele Speaker: ${data.teleSpeakerAvg.toStringAsFixed(1)}"),
+                        Text(
+                            "Avg Total Speaker: ${(data.teleSpeakerAvg + data.autoSpeakerAvg).toStringAsFixed(1)}"),
+                        Text(
+                            "Max Total Speaker: ${(data.bestSpeakerGamepiecesSum).toStringAsFixed(1)}"),
                       ],
                     ),
-                    const Column(
+                    Column(
                       children: <Widget>[
-                        Text(
-                          "Points",
+                        const Text(
+                          "Climb",
                           style: TextStyle(fontSize: 18),
                         ),
-                        //TODO points quickdata
+                        Text(
+                          "Climb Percentage: ${data.climbPercentage.toStringAsFixed(1)}%",
+                        ),
+                        Text(
+                          "Can Harmony: ${data.canHarmony ?? "No Data"}",
+                        ),
+                        Text("Matches Climbed 1: ${data.matchesClimbedSingle}"),
+                        Text("Matches Climbed 2: ${data.matchesClimbedDouble}"),
+                        Text("Matches Climbed 3: ${data.matchesClimbedTriple}"),
                       ],
                     ),
-                    const Column(
+                    Column(
                       children: <Widget>[
-                        Text(
+                        const Text(
                           "Misc",
                           style: TextStyle(fontSize: 18),
                         ),
-                        //TODO misc quickdata
+                        Text(
+                          "Avg Gamepiece Points: ${data.gamepiecePoints.toStringAsFixed(1)}",
+                        ),
+                        Text(
+                          "Possible Traps: ${data.trapAmount ?? "No Data"}",
+                        ),
+                        Text(
+                          "Avg Trap Amount: ${data.avgTrapAmount.toStringAsFixed(1)}",
+                        ),
                       ],
                     ),
-                    const Column(
+                    Column(
                       children: <Widget>[
-                        Text(
+                        const Text(
                           "Defense Stats",
                           style: TextStyle(fontSize: 18),
                         ),
-                        //TODO defense quickdata
+                        Text(
+                          "Avg GP Not Defending: ${data.avgGamepiecesNoDefense.isNaN ? "No Data" : data.avgGamepiecesNoDefense.toStringAsFixed(1)}",
+                        ),
+                        Text(
+                          "Avg GP Half Defending: ${data.avgGamepiecesHalfDefense.isNaN ? "No Data" : data.avgGamepiecesHalfDefense.toStringAsFixed(1)}",
+                        ),
+                        Text(
+                          "Avg GP Full Defending: ${data.avgGamepiecesFullDefense.isNaN ? "No Data" : data.avgGamepiecesFullDefense.toStringAsFixed(1)}",
+                        ),
                       ],
                     ),
-                    const Column(
+                    Column(
                       children: <Widget>[
-                        Text(
+                        const Text(
                           "Picklist",
                           style: TextStyle(fontSize: 18),
                         ),
-                        //TODO picklist quickdata
+                        Text(
+                          "1st Picklist Position: ${data.firstPicklistIndex + 1}",
+                        ),
+                        Text(
+                          "2nd Picklist Position: ${data.secondPicklistIndex + 1}",
+                        ),
+                        Text(
+                          "3rd Picklist Position: ${data.thirdPicklistIndex + 1}",
+                        ),
                       ],
                     ),
                   ]
