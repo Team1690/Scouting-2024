6d37dfa8396f7eecbca9a8945e21c058e88f9c29
IdoDePro <idozipori08@gmail.com>
2024-02-11 21:46:20 +0200

    coach quick data works

--- lib/views/mobile/screens/coach_team_info/coach_quick_data.dart
+++ lib/views/mobile/screens/coach_team_info/coach_quick_data.dart
@@ -16,26 +16,98 @@ class CoachQuickData extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               //TODO add quickdata ui here
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "Auto",
-                  style: TextStyle(fontSize: 18),
-                ),
+              const Text(
+                "Amp",
+                style: TextStyle(fontSize: 18),
               ),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "Teleop",
-                  style: TextStyle(fontSize: 18),
-                ),
+              Text(
+                "Median Auto Amp: ${data.medianData.autoAmp.toStringAsFixed(2)}",
               ),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "Points",
-                  style: TextStyle(fontSize: 18),
-                ),
+              Text(
+                "Median Tele Amp: ${data.medianData.teleAmp.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Avg Auto Amp: ${data.avgData.autoAmp.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Avg Tele Amp: ${data.avgData.autoSpeaker.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Avg Total Amp: ${(data.avgData.ampGamepieces).toStringAsFixed(2)}",
+              ),
+              Text(
+                "Max Total Amp: ${(data.maxData.ampGamepieces).toStringAsFixed(2)}",
+              ),
+              Text(
+                "Min Total Amp: ${data.minData.ampGamepieces.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Avg Auto Amp Missed: ${data.avgData.autoAmpMissed.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Avg Tele Amp Missed ${data.avgData.teleAmpMissed.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Median auto Amp Missed: ${data.medianData.autoAmpMissed.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Median Tele Amp Missed: ${data.medianData.teleAmpMissed.toStringAsFixed(2)}",
+              ),
+              const Text(
+                "Speaker",
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                "Median Auto Speaker: ${data.medianData.autoSpeaker.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Median Tele Speaker: ${data.medianData.teleSpeaker.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Avg Auto Speaker: ${data.avgData.autoSpeaker.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Avg Tele Speaker: ${data.avgData.teleSpeaker.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Avg Total Speaker: ${data.avgData.speakerGamepieces.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Max Total Speaker: ${(data.maxData.speakerGamepieces).toStringAsFixed(2)}",
+              ),
+              Text(
+                "Min Total Speaker: ${data.minData.speakerGamepieces.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Avg Auto Speaker Missed: ${data.avgData.autoSpeakerMissed.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Avg Tele Speaker Missed ${data.avgData.teleSpeakerMissed.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Median auto Speaker Missed: ${data.medianData.autoSpeakerMissed.toStringAsFixed(2)}",
+              ),
+              Text(
+                "Median Tele Speaker Missed: ${data.medianData.teleSpeakerMissed.toStringAsFixed(2)}",
+              ),
+              const Text(
+                "Climb",
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                "Climb Percentage: ${data.climbPercentage.isNaN ? "No Data" : data.climbPercentage.toStringAsFixed(1)}%",
+              ),
+              Text(
+                "Can Harmony: ${data.canHarmony ?? "No Data"}",
+              ),
+              Text(
+                "Matches Climbed 1: ${data.matchesClimbedSingle}",
+              ),
+              Text(
+                "Matches Climbed 2: ${data.matchesClimbedDouble}",
+              ),
+              Text(
+                "Matches Climbed 3: ${data.matchesClimbedTriple}",
               ),
               const Padding(
                 padding: EdgeInsets.all(10.0),
@@ -45,15 +117,32 @@ class CoachQuickData extends StatelessWidget {
                 ),
               ),
               Text(
+                "Median Gamepiece Points: ${data.medianData.gamePiecesPoints.toStringAsFixed(1)}",
+              ),
+              Text(
+                "Median Gamepiece: ${data.medianData.gamepieces.toStringAsFixed(1)}",
+              ),
+              Text(
+                "Avg Gamepiece Points: ${data.gamepiecePoints.toStringAsFixed(1)}",
+              ),
+              Text(
+                "Avg Gamepieces: ${data.gamepiecesScored.toStringAsFixed(1)}",
+              ),
+              Text(
+                "Possible Traps: ${data.trapAmount ?? "No Data"}",
+              ),
+              Text(
+                "Avg Trap Amount: ${data.avgData.trapAmount.toStringAsFixed(1)}",
+              ),
+              Text(
+                "Trap Success Rate: ${data.trapSuccessRate.isNaN ? "No Data" : "${data.trapSuccessRate.toStringAsFixed(1)}%"}",
+              ),
+              Text(
+                "Median Trap Amount: ${data.medianData.trapAmount}",
+              ),
+              Text(
                 textAlign: TextAlign.center,
                 "Matches Played: ${data.amoutOfMatches}",
-              ),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "Defense Stats",
-                  style: TextStyle(fontSize: 18),
-                ),
               ),
               const Padding(
                 padding: EdgeInsets.all(10.0),
@@ -69,6 +158,10 @@ class CoachQuickData extends StatelessWidget {
               Text(
                 textAlign: TextAlign.center,
                 "Second: ${data.secondPicklistIndex + 1}",
+              ),
+              Text(
+                textAlign: TextAlign.center,
+                "Second: ${data.thirdPicklistIndex + 1}",
               ),
             ],
           ),
--- lib/views/mobile/screens/coach_team_info/coach_team_info_data.dart
+++ lib/views/mobile/screens/coach_team_info/coach_team_info_data.dart
@@ -1,8 +1,15 @@
 import "package:flutter/material.dart";
 import "package:scouting_frontend/models/fetch_functions/fetch_single_team.dart";
 import "package:scouting_frontend/models/team_data/team_data.dart";
+import "package:scouting_frontend/models/team_data/team_match_data.dart";
+import "package:scouting_frontend/models/team_info_models/quick_data.dart";
 import "package:scouting_frontend/models/team_model.dart";
 import "package:orbit_standard_library/orbit_standard_library.dart";
+import "package:scouting_frontend/views/common/card.dart";
+import "package:scouting_frontend/views/mobile/screens/coach_team_info/coach_quick_data.dart";
+import "package:scouting_frontend/views/mobile/screens/coach_team_info/coach_team_info_line_charts.dart";
+import "package:scouting_frontend/views/pc/team_info/widgets/pit/pit_scouting_card.dart";
+import "package:scouting_frontend/views/pc/team_info/widgets/specific/specific_card.dart";
 
 class CoachTeamData extends StatelessWidget {
   const CoachTeamData(this.team);
@@ -36,55 +43,75 @@ class CoachTeamData extends StatelessWidget {
                     (final TeamData data) => CarouselWithIndicator(
                       enableInfininteScroll: true,
                       initialPage: 0,
-                      widgets: const <Widget>[
-                        // Padding(
-                        //   padding: const EdgeInsets.all(10.0),
-                        //   child: DashboardCard(
-                        //     title: "Line charts",
-                        //     body: data.quickData.amoutOfMatches < 2
-                        //         ? const Center(
-                        //             child: Text("No data :("),
-                        //           )
-                        //         : CoachTeamInfoLineCharts(data),
-                        //   ),
-                        // ),
-                        // Padding(
-                        //   padding: const EdgeInsets.all(10.0),
-                        //   child: DashboardCard(
-                        //     title: "Specific",
-                        //     body: data.specificData.msg.isEmpty
-                        //         ? const Center(
-                        //             child: Text("No data :("),
-                        //           )
-                        //         : SpecificCard(data.specificData),
-                        //   ),
-                        // ),
-                        // Padding(
-                        //   padding: const EdgeInsets.all(10),
-                        //   child: DashboardCard(
-                        //     title: "Auto Data",
-                        //     body: CoachAutoData(data.autoData),
-                        //   ),
-                        // ),
-                        // Padding(
-                        //   padding: const EdgeInsets.all(10),
-                        //   child: DashboardCard(
-                        //     title: "Quick data",
-                        //     body: CoachQuickData(data.quickData),
-                        //   ),
-                        // ),
-                        // Padding(
-                        //   padding: const EdgeInsets.all(10),
-                        //   child: data.pitViewData.mapNullable(
-                        //         PitScoutingCard.new,
-                        //       ) ??
-                        //       const DashboardCard(
-                        //         title: "Pit scouting",
-                        //         body: Center(
-                        //           child: Text("No data"),
-                        //         ),
-                        //       ),
-                        // ),
+                      widgets: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: DashboardCard(
+                            title: "Line charts",
+                            body: data.aggregateData.gamesPlayed < 2
+                                ? const Center(
+                                    child: Text("No data :("),
+                                  )
+                                : CoachTeamInfoLineCharts(data),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: DashboardCard(
+                            title: "Specific",
+                            body: data.matches.specificMatches.isEmpty
+                                ? const Center(
+                                    child: Text("No data :("),
+                                  )
+                                : SpecificCard(
+                                    matchData: data.matches.specificMatches,
+                                    summaryData: data.summaryData,
+                                  ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: DashboardCard(
+                            title: "Quick data",
+                            body: CoachQuickData(
+                              QuickData(
+                                medianData: data.aggregateData.medianData,
+                                avgData: data.aggregateData.avgData,
+                                maxData: data.aggregateData.maxData,
+                                minData: data.aggregateData.minData,
+                                amoutOfMatches: data.aggregateData.gamesPlayed,
+                                firstPicklistIndex: data.firstPicklistIndex,
+                                secondPicklistIndex: data.secondPicklistIndex,
+                                thirdPicklistIndex: data.thirdPicklistIndex,
+                                //TODO: getters...
+                                matchesClimbedSingle: 0,
+                                matchesClimbedDouble: 0,
+                                matchesClimbedTriple: 0,
+                                climbPercentage: data.climbPercentage,
+                                canHarmony: data.pitData?.harmony,
+                                gamepiecePoints:
+                                    data.aggregateData.avgData.gamePiecesPoints,
+                                gamepiecesScored:
+                                    data.aggregateData.avgData.gamepieces,
+                                trapAmount:
+                                    data.aggregateData.sumData.trapAmount,
+                                trapSuccessRate: 0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: data.pitData.mapNullable(
+                                PitScoutingCard.new,
+                              ) ??
+                              const DashboardCard(
+                                title: "Pit scouting",
+                                body: Center(
+                                  child: Text("No data"),
+                                ),
+                              ),
+                        ),
                       ],
                     ),
                   ) ??
