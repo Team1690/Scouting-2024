c6346eb7a4e416335510056803493bd2d70bda36
amitcha06 <amitchayko@gmail.com>
2024-02-20 21:59:32 +0200

    altered the fetch in order to obtain the match urgency

--- lib/views/mobile/screens/fault_view.dart
+++ lib/views/mobile/screens/fault_view.dart
@@ -1,9 +1,13 @@
+import "package:collection/collection.dart";
 import "package:flutter/material.dart";
 import "package:graphql/client.dart";
 import "package:orbit_standard_library/orbit_standard_library.dart";
 import "package:scouting_frontend/models/enums/fault_status_enum.dart";
+import "package:scouting_frontend/models/schedule_match.dart";
+import "package:scouting_frontend/models/team_model.dart";
 import "package:scouting_frontend/net/hasura_helper.dart";
 import "package:scouting_frontend/views/constants.dart";
+import "package:scouting_frontend/views/mobile/screens/coach_view.dart";
 import "package:scouting_frontend/views/mobile/screens/fault_view/add_fault.dart";
 import "package:scouting_frontend/views/mobile/screens/fault_view/fault_tile.dart";
 import "package:scouting_frontend/views/mobile/side_nav_bar.dart";
@@ -22,11 +26,11 @@ class FaultView extends StatelessWidget {
             ),
           ],
         ),
-        body: StreamBuilder<List<FaultEntry>>(
+        body: StreamBuilder<List<(FaultEntry, int?)>>(
           stream: fetchFaults(),
           builder: (
             final BuildContext context,
-            final AsyncSnapshot<List<FaultEntry>> snapshot,
+            final AsyncSnapshot<List<(FaultEntry, int?)>> snapshot,
           ) {
             if (snapshot.hasError) {
               return Text(snapshot.error.toString());
@@ -36,12 +40,13 @@ class FaultView extends StatelessWidget {
               );
             } else {
               return snapshot.data.mapNullable(
-                    (final List<FaultEntry> data) => SingleChildScrollView(
+                    (final List<(FaultEntry, int?)> data) =>
+                        SingleChildScrollView(
                       primary: false,
                       child: Column(
                         children: data
                             .map(
-                              (final FaultEntry e) => Card(
+                              (final (FaultEntry, int?) e) => Card(
                                 elevation: 2,
                                 color: bgColor,
                                 child: Padding(
@@ -91,13 +96,47 @@ void showLoadingSnackBar(final BuildContext context) =>
       ),
     );
 
-Stream<List<FaultEntry>> fetchFaults() {
+Stream<List<(FaultEntry, int?)>> fetchFaults() {
   final GraphQLClient client = getClient();
-  final Stream<QueryResult<List<FaultEntry>>> result = client.subscribe(
-    SubscriptionOptions<List<FaultEntry>>(
+  final Stream<QueryResult<List<(FaultEntry, int?)>>> result = client.subscribe(
+    SubscriptionOptions<List<(FaultEntry, int?)>>(
       document: gql(subscription),
       parserFn: (final Map<String, dynamic> data) =>
-          (data["faults"] as List<dynamic>).map(FaultEntry.parse).toList(),
+          (data["schedule_matches"] as List<dynamic>)
+              .map((e) => (e["faults"] as List<dynamic>).map(
+                    (fault) {
+                      final int lastMatch =
+                          (data["schedule_matches"] as List<dynamic>)
+                              .lastIndexWhere(
+                        (element) => element["happened"] as bool,
+                      );
+                      final dynamic nextMatch =
+                          (data["schedule_matches"] as List<dynamic>)
+                              .where(
+                                (final dynamic element) => teamValues.any(
+                                    (final String team) =>
+                                        element[team]?["number"] == 1690),
+                              )
+                              .firstWhereOrNull(
+                                (final dynamic element) => teamValues.any(
+                                    (final String team) =>
+                                        element[team]?["number"] ==
+                                        LightTeam.fromJson(fault["team"])
+                                            .number),
+                              );
+                      return (
+                        FaultEntry.parse(fault),
+                        nextMatch == null
+                            ? null
+                            : lastMatch -
+                                ScheduleMatch.fromJson(nextMatch, false)
+                                    .matchIdentifier
+                                    .number
+                      );
+                    },
+                  ))
+              .flattened
+              .toList(),
     ),
   );
   return result.map(queryResultToParsed);
@@ -118,10 +157,11 @@ class NewFault {
   final bool isRematch;
 }
 
-const String subscription = """
+String subscription = """
 subscription MyQuery {
-  faults(order_by: {fault_status: {order: asc}}) {
-    fault_status {
+  schedule_matches(order_by: {match_type: {order: asc}, match_number: asc}) {
+        faults(order_by: {fault_status: {order: asc}}) {
+      fault_status {
       title
     }
     id
@@ -139,6 +179,24 @@ subscription MyQuery {
       match_number
     }
     is_rematch
+    
+    }
+    id
+    happened
+    match_number
+    match_type {
+      title
+    }
+    ${teamValues.map(
+          (final String e) => """$e{
+      colors_index
+      id
+      name
+      number
+    
+    }
+    """,
+        ).join(" ")}
   }
 }
 """;
