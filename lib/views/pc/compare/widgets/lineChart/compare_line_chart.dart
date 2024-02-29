e52b9c050a60bacf02fa02a13d021ef924482493
IdoDePro <idozipori08@gmail.com>
2024-02-29 18:23:07 +0200

    lints

--- lib/views/pc/compare/widgets/lineChart/compare_line_chart.dart
+++ lib/views/pc/compare/widgets/lineChart/compare_line_chart.dart
@@ -1,7 +1,4 @@
-import "dart:math";
 import "package:flutter/material.dart";
-import "package:scouting_frontend/models/enums/match_type_enum.dart";
-import "package:scouting_frontend/models/match_identifier.dart";
 import "package:scouting_frontend/models/data/team_data/team_data.dart";
 import "package:scouting_frontend/models/data/technical_match_data.dart";
 import "package:scouting_frontend/views/common/dashboard_linechart.dart";
