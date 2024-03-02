a70734ed4b6b221c59f72debc28674748c8e83f7
IdoDePro <idozipori08@gmail.com>
2024-03-02 12:31:46 +0200

    moved all the state to a different widget so that you don't need to fetch again

--- lib/views/pc/status/status.dart
+++ lib/views/pc/status/status.dart
@@ -1,27 +1,14 @@
 import "dart:collection";
-import "package:collection/collection.dart";
 import "package:flutter/material.dart";
 import "package:scouting_frontend/models/data/team_data/team_data.dart";
-import "package:scouting_frontend/models/data/team_match_data.dart";
 import "package:scouting_frontend/models/fetch_functions/fetch_teams.dart";
 import "package:scouting_frontend/models/id_providers.dart";
 import "package:scouting_frontend/models/team_model.dart";
 import "package:scouting_frontend/net/hasura_helper.dart";
 import "package:scouting_frontend/views/common/dashboard_scaffold.dart";
-import "package:scouting_frontend/views/constants.dart";
-import "package:scouting_frontend/views/pc/status/new_status_list.dart";
-import "package:scouting_frontend/views/pc/status/widgets/status_box.dart";
+import "package:scouting_frontend/views/pc/status/status_card.dart";
 
-class Status extends StatefulWidget {
-  @override
-  State<Status> createState() => _StatusState();
-}
-
-class _StatusState extends State<Status> {
-  bool isSpecific = false;
-
-  bool isPreScouting = false;
-
+class Status extends StatelessWidget {
   @override
   Widget build(final BuildContext context) => DashboardScaffold(
         body: StreamBuilder<SplayTreeSet<TeamData>>(
@@ -37,90 +24,8 @@ class _StatusState extends State<Status> {
             final AsyncSnapshot<SplayTreeSet<TeamData>> snapshot,
           ) =>
               snapshot.mapSnapshot(
-            onSuccess: (final SplayTreeSet<TeamData> data) => Center(
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(defaultPadding),
-                        child: ToggleButtons(
-                          children: const <Widget>[
-                            Text("Technical"),
-                            Text("Specific"),
-                            Text("Pre Scouting"),
-                          ],
-                          isSelected: <bool>[
-                            !isSpecific,
-                            isSpecific,
-                            isPreScouting,
-                          ],
-                          onPressed: (final int index) {
-                            setState(() {
-                              switch (index) {
-                                case 0:
-                                  isSpecific = false;
-                                  break;
-                                case 1:
-                                  isSpecific = true;
-                                  break;
-                                case 2:
-                                  isPreScouting = !isPreScouting;
-                                  break;
-                              }
-                            });
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: StatusList<Object>(
-                      data: data
-                          .map((final TeamData element) => element.matches)
-                          .map(
-                            (final List<MatchData> e) => isSpecific
-                                ? e.specificMatches
-                                : e.technicalMatchExists,
-                          )
-                          .flattened
-                          .toList(),
-                      groupBy: (final MatchData matchData) => isPreScouting
-                          ? matchData.team
-                          : matchData.scheduleMatch.matchIdentifier,
-                      orderBy: (final MatchData p0, final MatchData p1) => p1
-                          .scheduleMatch.matchIdentifier.number
-                          .compareTo(p0.scheduleMatch.matchIdentifier.number),
-                      leading: (final List<MatchData> row) => Text(
-                        row.first.scheduleMatch.matchIdentifier.toString(),
-                      ),
-                      statusBoxBuilder: (final MatchData data) => StatusBox(
-                        backgroundColor:
-                            data.scheduleMatch.blueAlliance.contains(data.team)
-                                ? Colors.blue
-                                : Colors.red,
-                        child: Column(
-                          children: <Widget>[
-                            Text(data.team.number.toString()),
-                            Text(
-                              isSpecific
-                                  ? data.specificMatchData!.scouterName
-                                  : data.technicalMatchData!.scouterName,
-                            ),
-                            if (!isSpecific)
-                              Text(
-                                data.technicalMatchData!.data.gamePiecesPoints
-                                    .toString(),
-                              ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            onSuccess: (final SplayTreeSet<TeamData> data) =>
+                StatusCard(data: data.toList()),
             onWaiting: () => const Center(
               child: CircularProgressIndicator(),
             ),
---
+++ lib/views/pc/status/status_card.dart
@@ -0,0 +1,105 @@
+import "package:collection/collection.dart";
+import "package:flutter/material.dart";
+import "package:scouting_frontend/models/data/team_data/team_data.dart";
+import "package:scouting_frontend/models/data/team_match_data.dart";
+import "package:scouting_frontend/views/constants.dart";
+import "package:scouting_frontend/views/pc/status/new_status_list.dart";
+import "package:scouting_frontend/views/pc/status/widgets/status_box.dart";
+
+class StatusCard extends StatefulWidget {
+  const StatusCard({super.key, required this.data});
+
+  final List<TeamData> data;
+  @override
+  State<StatusCard> createState() => _StatusCardState();
+}
+
+class _StatusCardState extends State<StatusCard> {
+  bool isSpecific = false;
+  bool isPreScouting = false;
+  @override
+  Widget build(final BuildContext context) => Center(
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(defaultPadding),
+                  child: ToggleButtons(
+                    children: const <Widget>[
+                      Text("Technical"),
+                      Text("Specific"),
+                      Text("Pre Scouting"),
+                    ],
+                    isSelected: <bool>[
+                      !isSpecific,
+                      isSpecific,
+                      isPreScouting,
+                    ],
+                    onPressed: (final int index) {
+                      setState(() {
+                        switch (index) {
+                          case 0:
+                            isSpecific = false;
+                            break;
+                          case 1:
+                            isSpecific = true;
+                            break;
+                          case 2:
+                            isPreScouting = !isPreScouting;
+                            break;
+                        }
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: StatusList<Object>(
+                data: widget.data
+                    .map((final TeamData element) => element.matches)
+                    .map(
+                      (final List<MatchData> e) => isSpecific
+                          ? e.specificMatches
+                          : e.technicalMatchExists,
+                    )
+                    .flattened
+                    .toList(),
+                groupBy: (final MatchData matchData) => isPreScouting
+                    ? matchData.team
+                    : matchData.scheduleMatch.matchIdentifier,
+                orderBy: (final MatchData p0, final MatchData p1) => p1
+                    .scheduleMatch.matchIdentifier.number
+                    .compareTo(p0.scheduleMatch.matchIdentifier.number),
+                leading: (final List<MatchData> row) => Text(
+                  row.first.scheduleMatch.matchIdentifier.toString(),
+                ),
+                statusBoxBuilder: (final MatchData data) => StatusBox(
+                  backgroundColor:
+                      data.scheduleMatch.blueAlliance.contains(data.team)
+                          ? Colors.blue
+                          : Colors.red,
+                  child: Column(
+                    children: <Widget>[
+                      Text(data.team.number.toString()),
+                      Text(
+                        isSpecific
+                            ? data.specificMatchData!.scouterName
+                            : data.technicalMatchData!.scouterName,
+                      ),
+                      if (!isSpecific)
+                        Text(
+                          data.technicalMatchData!.data.gamePiecesPoints
+                              .toString(),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+}
