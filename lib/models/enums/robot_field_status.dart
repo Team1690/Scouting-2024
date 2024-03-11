8f37f70301deb3f2076b12d2a431a02d367a7444
IdoDePro <idozipori08@gmail.com>
2024-03-11 19:58:20 +0200

    redid robotfieldstatus enum

--- lib/models/enums/robot_field_status.dart
+++ lib/models/enums/robot_field_status.dart
@@ -1,21 +1,19 @@
-enum RobotFieldStatus {
-  worked("Worked"),
-  didntComeToField("Didn't come to field"),
-  didntWorkOnField("Didn't work on field");
+import "package:flutter/material.dart";
 
-  const RobotFieldStatus(this.title);
+enum RobotFieldStatus {
+  worked("Worked", Colors.green),
+  didntComeToField("Didn't come to field", Colors.red),
+  didntWorkOnField("Didn't work on field", Colors.purple),
+  didDefense("Did Defense", Colors.blue);
+
+  const RobotFieldStatus(this.title, this.color);
 
   final String title;
+  final Color color;
 }
 
-RobotFieldStatus robotFieldStatusTitleToEnum(final String title) {
-  switch (title) {
-    case "Worked":
-      return RobotFieldStatus.worked;
-    case "Didn't come to field":
-      return RobotFieldStatus.didntComeToField;
-    case "Didn't work on field":
-      return RobotFieldStatus.didntWorkOnField;
-  }
-  throw Exception("Isn't a valid title");
-}
+RobotFieldStatus robotFieldStatusTitleToEnum(final String title) =>
+    RobotFieldStatus.values
+        .where((final RobotFieldStatus element) => element.title == title)
+        .singleOrNull ??
+    (throw Exception("Not A Valid Robot Field Status Title"));
