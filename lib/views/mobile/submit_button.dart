10e2b598f49c55c594433fac83b0d99c6b21d7c4
amitaskof <amit.askof@gmail.com>
2024-03-17 19:07:21 +0200

    Ignore print lint

--- lib/views/mobile/submit_button.dart
+++ lib/views/mobile/submit_button.dart
@@ -104,6 +104,7 @@ class _SubmitButtonState extends State<SubmitButton> {
             print(errors.map((final GraphQLError e) => (e.message)).toList());
             if (errors.length == 1) {
               final GraphQLError error = errors.single;
+              // ignore: avoid_print
               print(error.message);
               _errorMessage = error.extensions?["code"]?.toString() ==
                       "constraint-violation"
