2aff1725eb4a5ca951a1ab341a63267c1fde4320
IdoDePro <idozipori08@gmail.com>
2024-03-30 22:03:10 +0300

    added option to delete scouters

--- lib/views/pc/scouting_shifts/widgets/edit_scouters_button.dart
+++ lib/views/pc/scouting_shifts/widgets/edit_scouters_button.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 import "package:scouting_frontend/net/hasura_helper.dart";
 import "package:scouting_frontend/views/pc/scouting_shifts/queries/add_scouter.dart";
+import "package:scouting_frontend/views/pc/scouting_shifts/queries/delete_scouter.dart";
 import "package:scouting_frontend/views/pc/scouting_shifts/queries/fetch_scouters.dart";
 import "package:scouting_frontend/views/pc/scouting_shifts/queries/update_scouter_name.dart";
 
@@ -19,55 +21,70 @@ class _EditScoutersButtonState extends State<EditScoutersButton> {
           showDialog(
             context: context,
             builder: (final BuildContext context) => AlertDialog(
-              content: Column(
-                children: <Widget>[
-                  TextField(
-                    controller: addController,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      if (addController.text != "") {
-                        addScouter(addController.text);
-                      }
-                      addController.clear();
-                    },
-                    icon: const Text("Add"),
-                  ),
-                  StreamBuilder<List<String>>(
-                    stream: fetchScouters(),
-                    builder: (
-                      final BuildContext context,
-                      final AsyncSnapshot<List<String>> snapshot,
-                    ) =>
-                        snapshot.mapSnapshot(
-                      onSuccess: (final List<String> data) =>
-                          SingleChildScrollView(
-                              child: Column(
-                        children: data
-                            .map(
-                              ((final String e) => TextField(
-                                    controller: TextEditingController(text: e),
-                                    onSubmitted: (final String value) {
-                                      updateScouter(e, value);
-                                    },
-                                  )),
-                            )
-                            .toList(),
-                      )),
-                      onWaiting: () => const Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                      onNoData: () => const Text("No Data"),
-                      onError: (final Object error) => Center(
-                        child: Text(error.toString()),
+              content: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: addController,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        if (addController.text != "") {
+                          addScouter(addController.text);
+                        }
+                        addController.clear();
+                      },
+                      icon: const Icon(Icons.person_add_alt_1),
+                    ),
+                    StreamBuilder<List<String>>(
+                      stream: fetchScouters(),
+                      builder: (
+                        final BuildContext context,
+                        final AsyncSnapshot<List<String>> snapshot,
+                      ) =>
+                          snapshot.mapSnapshot(
+                        onSuccess: (final List<String> data) =>
+                            SingleChildScrollView(
+                                child: Column(
+                          children: data
+                              .map(
+                                ((final String scouterName) => Row(
+                                      children: [
+                                        Expanded(
+                                          child: TextField(
+                                            controller: TextEditingController(
+                                                text: scouterName),
+                                            onSubmitted: (final String value) {
+                                              updateScouter(scouterName, value);
+                                            },
+                                          ),
+                                        ),
+                                        IconButton(
+                                          onPressed: () {
+                                            deleteScouter(scouterName);
+                                          },
+                                          icon: const Icon(Icons.group_remove),
+                                        )
+                                      ],
+                                    )),
+                              )
+                              .toList(),
+                        )),
+                        onWaiting: () => const Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                        onNoData: () => const Text("No Data"),
+                        onError: (final Object error) => Center(
+                          child: Text(error.toString()),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           );
         },
-        icon: const Icon(Icons.add),
+        icon: const Icon(Icons.manage_accounts),
       );
 }
