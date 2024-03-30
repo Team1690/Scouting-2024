e68a068bb5547e087739f6d1423ad9d4cf3c4e7c
IdoDePro <idozipori08@gmail.com>
2024-03-30 16:14:15 +0300

    added an add scouter button

--- lib/views/pc/scouting_shifts/scouting_shifts_screen.dart
+++ lib/views/pc/scouting_shifts/scouting_shifts_screen.dart
@@ -1,9 +1,11 @@
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:flutter/widgets.dart";
 import "package:scouting_frontend/net/hasura_helper.dart";
 import "package:scouting_frontend/views/common/dashboard_scaffold.dart";
 import "package:scouting_frontend/views/pc/scouting_shifts/fetch_shifts.dart";
 import "package:scouting_frontend/views/pc/scouting_shifts/scouting_shift.dart";
+import "package:scouting_frontend/views/pc/scouting_shifts/widgets/add_scouter_button.dart";
 
 class ScoutingShiftsScreen extends StatelessWidget {
   const ScoutingShiftsScreen({super.key});
@@ -11,32 +13,39 @@ class ScoutingShiftsScreen extends StatelessWidget {
   //TODO: mapSnapshot needs a default value for nodata error and on waiting, so to get rid of duplicate code
   @override
   Widget build(final BuildContext context) => DashboardScaffold(
-        body: StreamBuilder<List<ScoutingShift>>(
-          stream: fetchShifts(context),
-          builder: (
-            final BuildContext context,
-            final AsyncSnapshot<List<ScoutingShift>> snapshot,
-          ) =>
-              snapshot.mapSnapshot(
-            onSuccess: (final List<ScoutingShift> data) => ListView(
-              children: data
-                  .map(
-                    (e) => ListTile(
-                      title: Text(
-                        "${e.name} ${e.team.number} ${e.matchIdentifier}",
-                      ),
-                    ),
-                  )
-                  .toList(),
+        body: Column(
+          children: <Widget>[
+            const Row(
+              children: <Widget>[AddScouterButton()],
             ),
-            onWaiting: () => const Center(
-              child: CircularProgressIndicator(),
+            StreamBuilder<List<ScoutingShift>>(
+              stream: fetchShifts(context),
+              builder: (
+                final BuildContext context,
+                final AsyncSnapshot<List<ScoutingShift>> snapshot,
+              ) =>
+                  snapshot.mapSnapshot(
+                onSuccess: (final List<ScoutingShift> data) => ListView(
+                  children: data
+                      .map(
+                        (final ScoutingShift e) => ListTile(
+                          title: Text(
+                            "${e.name} ${e.team.number} ${e.matchIdentifier}",
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+                onWaiting: () => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+                onNoData: () => const Text("No Data"),
+                onError: (final Object error) => Center(
+                  child: Text(error.toString()),
+                ),
+              ),
             ),
-            onNoData: () => const Text("No Data"),
-            onError: (final Object error) => Center(
-              child: Text(error.toString()),
-            ),
-          ),
+          ],
         ),
       );
 }
