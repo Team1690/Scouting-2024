2d14365bf2dcdad0f73f726e9bee71ec6d8280c3
IdoDePro <idozipori08@gmail.com>
2024-04-03 21:50:17 +0300

    added button to scatter that shows 'normalized' data

--- lib/views/pc/scatter/scatter.dart
+++ lib/views/pc/scatter/scatter.dart
@@ -4,6 +4,7 @@ import "package:flutter/material.dart";
 import "package:scouting_frontend/models/fetch_functions/fetch_all_teams.dart";
 import "package:scouting_frontend/models/data/all_team_data.dart";
 import "package:scouting_frontend/models/team_model.dart";
+import "package:scouting_frontend/net/hasura_helper.dart";
 import "package:scouting_frontend/views/common/card.dart";
 import "package:scouting_frontend/views/constants.dart";
 import "package:orbit_standard_library/orbit_standard_library.dart";
@@ -15,6 +16,7 @@ class Scatter extends StatefulWidget {
 
 class _ScatterState extends State<Scatter> {
   bool isPoints = true;
+  bool normalize = false;
   @override
   Widget build(final BuildContext context) {
     String? tooltip;
@@ -48,6 +50,13 @@ class _ScatterState extends State<Scatter> {
             }
           },
         ),
+        IconButton(
+            onPressed: () {
+              setState(() {
+                normalize = !normalize;
+              });
+            },
+            icon: const Icon(Icons.auto_fix_normal)),
       ],
       body: Column(
         children: <Widget>[
@@ -58,183 +67,185 @@ class _ScatterState extends State<Scatter> {
               builder: (
                 final BuildContext context,
                 final AsyncSnapshot<List<AllTeamData>> snapshot,
-              ) {
-                if (snapshot.hasError) {
-                  return Text(
-                    "Error has happened in the future! ${snapshot.error}",
-                  );
-                } else if (snapshot.connectionState ==
-                    ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else {
-                  return snapshot.data
-                          .mapNullable((final List<AllTeamData> rawReport) {
-                        final List<AllTeamData> report = rawReport
-                            .where(
-                              (final AllTeamData element) =>
-                                  element.technicalMatches.isNotEmpty,
-                            )
-                            .toList();
-                        if (report.isEmpty) {
-                          return const Text("No data");
-                        }
-                        final List<LightTeam> teams = report
-                            .map(
-                              (final AllTeamData e) => e.team,
-                            )
-                            .toList();
-                        return ScatterChart(
-                          ScatterChartData(
-                            scatterSpots: report
-                                .map(
-                                  (final AllTeamData e) => isPoints
-                                      ? ScatterSpot(
-                                          e.aggregateData.avgData
-                                              .gamePiecesPoints,
-                                          e.aggregateData.meanDeviationData
-                                              .gamePiecesPoints,
-                                          color: e.team.color,
-                                        )
-                                      : ScatterSpot(
-                                          e.aggregateData.avgData.gamepieces,
-                                          e.aggregateData.meanDeviationData
-                                              .gamepieces,
-                                          color: e.team.color,
-                                        ),
-                                )
-                                .toList(),
-                            scatterTouchData: ScatterTouchData(
-                              touchCallback: (
-                                final FlTouchEvent event,
-                                final ScatterTouchResponse? response,
-                              ) {
-                                if (response?.touchedSpot != null) {
-                                  tooltip =
-                                      teams[response!.touchedSpot!.spotIndex]
-                                          .number
-                                          .toString();
-                                }
-                              },
-                              enabled: true,
-                              handleBuiltInTouches: true,
-                              touchTooltipData: ScatterTouchTooltipData(
-                                tooltipBgColor: bgColor,
-                                getTooltipItems:
-                                    (final ScatterSpot touchedBarSpot) =>
-                                        ScatterTooltipItem(
-                                  tooltip!,
-                                  textStyle:
-                                      const TextStyle(color: Colors.white),
-                                  bottomMargin: 10,
-                                ),
-                              ),
-                            ),
-                            titlesData: FlTitlesData(
-                              show: true,
-                              topTitles: AxisTitles(
-                                sideTitles: SideTitles(showTitles: false),
-                              ),
-                              rightTitles: AxisTitles(
-                                sideTitles: SideTitles(showTitles: false),
-                              ),
-                              bottomTitles: AxisTitles(
-                                axisNameSize: 26,
-                                axisNameWidget: isPoints
-                                    ? const Text(
-                                        "Average gamepiece points",
-                                      )
-                                    : const Text("Average gamepieces"),
-                                sideTitles: SideTitles(
-                                  getTitlesWidget: (
-                                    final double title,
-                                    final TitleMeta meta,
-                                  ) =>
-                                      Container(
-                                    child: Text(title.toString()),
+              ) =>
+                  snapshot.mapSnapshot(
+                onSuccess: (final List<AllTeamData> data) {
+                  final List<AllTeamData> report = data
+                      .where(
+                        (final AllTeamData element) =>
+                            element.technicalMatches.isNotEmpty,
+                      )
+                      .toList();
+                  if (report.isEmpty) {
+                    return const Text("No data");
+                  }
+                  final List<LightTeam> teams = report
+                      .map(
+                        (final AllTeamData e) => e.team,
+                      )
+                      .toList();
+                  return ScatterChart(
+                    ScatterChartData(
+                      scatterSpots: report
+                          .map(
+                            (final AllTeamData e) => isPoints
+                                ? ScatterSpot(
+                                    normalize
+                                        ? e.aggregateData.avgData
+                                                .gamePiecesPoints -
+                                            e.aggregateData.meanDeviationData
+                                                .gamePiecesPoints
+                                        : e.aggregateData.avgData
+                                            .gamePiecesPoints,
+                                    e.aggregateData.meanDeviationData
+                                        .gamePiecesPoints,
+                                    color: e.team.color,
+                                  )
+                                : ScatterSpot(
+                                    normalize
+                                        ? e.aggregateData.avgData.gamepieces -
+                                            e.aggregateData.meanDeviationData
+                                                .gamepieces
+                                        : e.aggregateData.avgData.gamepieces,
+                                    e.aggregateData.meanDeviationData
+                                        .gamepieces,
+                                    color: e.team.color,
                                   ),
-                                  showTitles: true,
-                                  interval: 5,
-                                ),
-                              ),
-                              leftTitles: AxisTitles(
-                                axisNameSize: 26,
-                                axisNameWidget: isPoints
-                                    ? const Text(
-                                        "Gamepiece points standard deviation",
-                                      )
-                                    : const Text(
-                                        "Gamepieces standard deviation",
-                                      ),
-                                sideTitles: SideTitles(
-                                  getTitlesWidget: (
-                                    final double title,
-                                    final TitleMeta meta,
-                                  ) =>
-                                      Container(
-                                    child: Text(title.toString()),
-                                  ),
-                                  reservedSize: 22,
-                                  showTitles: true,
-                                  interval: 5,
-                                ),
-                              ),
-                            ),
-                            gridData: FlGridData(
-                              show: true,
-                              horizontalInterval: 5,
-                              verticalInterval: 5,
-                              drawHorizontalLine: true,
-                              checkToShowHorizontalLine: (final double value) =>
-                                  true,
-                              getDrawingHorizontalLine: (final double value) =>
-                                  FlLine(
-                                color: Colors.black.withOpacity(0.1),
-                              ),
-                              drawVerticalLine: true,
-                              checkToShowVerticalLine: (final double value) =>
-                                  true,
-                              getDrawingVerticalLine: (final double value) =>
-                                  FlLine(
-                                color: Colors.black.withOpacity(0.1),
-                              ),
-                            ),
-                            borderData: FlBorderData(
-                              show: true,
-                            ),
-                            minX: 0,
-                            minY: 0,
-                            maxX: report
-                                .map(
-                                  (final AllTeamData e) => isPoints
-                                      ? (e.aggregateData.avgData
-                                                  .gamePiecesPoints +
-                                              1)
-                                          .roundToDouble()
-                                      : (e.aggregateData.avgData.gamepieces + 1)
-                                          .roundToDouble(),
-                                )
-                                .max,
-                            maxY: report
-                                .map(
-                                  (final AllTeamData e) => isPoints
-                                      ? (e.aggregateData.meanDeviationData
-                                                  .gamePiecesPoints +
-                                              1)
-                                          .roundToDouble()
-                                      : (e.aggregateData.meanDeviationData
-                                                  .gamepieces +
-                                              1)
-                                          .roundToDouble(),
-                                )
-                                .max,
+                          )
+                          .toList(),
+                      scatterTouchData: ScatterTouchData(
+                        touchCallback: (
+                          final FlTouchEvent event,
+                          final ScatterTouchResponse? response,
+                        ) {
+                          if (response?.touchedSpot != null) {
+                            tooltip = teams[response!.touchedSpot!.spotIndex]
+                                .number
+                                .toString();
+                          }
+                        },
+                        enabled: true,
+                        handleBuiltInTouches: true,
+                        touchTooltipData: ScatterTouchTooltipData(
+                          tooltipBgColor: bgColor,
+                          getTooltipItems: (final ScatterSpot touchedBarSpot) =>
+                              ScatterTooltipItem(
+                            tooltip!,
+                            textStyle: const TextStyle(color: Colors.white),
+                            bottomMargin: 10,
                           ),
-                        );
-                      }) ??
-                      (throw Exception("No data"));
-                }
-              },
+                        ),
+                      ),
+                      titlesData: FlTitlesData(
+                        show: true,
+                        topTitles: AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                        rightTitles: AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                        bottomTitles: AxisTitles(
+                          axisNameSize: 26,
+                          axisNameWidget: isPoints
+                              ? const Text(
+                                  "Average gamepiece points",
+                                )
+                              : const Text("Average gamepieces"),
+                          sideTitles: SideTitles(
+                            getTitlesWidget: (
+                              final double title,
+                              final TitleMeta meta,
+                            ) =>
+                                Container(
+                              child: Text(title.toString()),
+                            ),
+                            showTitles: true,
+                            interval: 5,
+                          ),
+                        ),
+                        leftTitles: AxisTitles(
+                          axisNameSize: 26,
+                          axisNameWidget: isPoints
+                              ? const Text(
+                                  "Gamepiece points standard deviation",
+                                )
+                              : const Text(
+                                  "Gamepieces standard deviation",
+                                ),
+                          sideTitles: SideTitles(
+                            getTitlesWidget: (
+                              final double title,
+                              final TitleMeta meta,
+                            ) =>
+                                Container(
+                              child: Text(title.toString()),
+                            ),
+                            reservedSize: 22,
+                            showTitles: true,
+                            interval: 5,
+                          ),
+                        ),
+                      ),
+                      gridData: FlGridData(
+                        show: true,
+                        horizontalInterval: 5,
+                        verticalInterval: 5,
+                        drawHorizontalLine: true,
+                        checkToShowHorizontalLine: (final double value) => true,
+                        getDrawingHorizontalLine: (final double value) =>
+                            FlLine(
+                          color: Colors.black.withOpacity(0.1),
+                        ),
+                        drawVerticalLine: true,
+                        checkToShowVerticalLine: (final double value) => true,
+                        getDrawingVerticalLine: (final double value) => FlLine(
+                          color: Colors.black.withOpacity(0.1),
+                        ),
+                      ),
+                      borderData: FlBorderData(
+                        show: true,
+                      ),
+                      minX: 0,
+                      minY: 0,
+                      maxX: report
+                          .map(
+                            (final AllTeamData e) => isPoints
+                                ? (e.aggregateData.avgData.gamePiecesPoints + 1)
+                                    .roundToDouble()
+                                : (e.aggregateData.avgData.gamepieces + 1)
+                                    .roundToDouble(),
+                          )
+                          .max,
+                      maxY: report
+                          .map(
+                            (final AllTeamData e) => isPoints
+                                ? (e.aggregateData.meanDeviationData
+                                            .gamePiecesPoints +
+                                        1)
+                                    .roundToDouble()
+                                : (e.aggregateData.meanDeviationData
+                                            .gamepieces +
+                                        1)
+                                    .roundToDouble(),
+                          )
+                          .max,
+                    ),
+                  );
+                },
+                onWaiting: () => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+                onNoData: () => const Center(
+                  child: Text(
+                    "No Data!",
+                  ),
+                ),
+                onError: (final Object error) => Center(
+                  child: Text(
+                    "Error has happened in the future! $error",
+                  ),
+                ),
+              ),
             ),
           ),
         ],
