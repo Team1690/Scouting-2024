90e2ba74cea536d5008403267cdfd75607e40a3a
BOSSkabab <amit.askof@gmail.com>
2024-06-23 22:08:08 +0300

    fixed the get event alert dialog

--- lib/views/pc/compare blue alliance/event_alert_dialog.dart
+++ lib/views/pc/compare blue alliance/event_alert_dialog.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 import "package:scouting_frontend/views/mobile/screens/pit_view/pit_toggle.dart";
+import "package:scouting_frontend/views/pc/compare%20blue%20alliance/blue_alliance_match_data.dart";
+import "package:scouting_frontend/views/pc/compare%20blue%20alliance/fetch_match_results.dart";
 
 class EventAlertDialog extends StatefulWidget {
   @override
@@ -7,7 +9,7 @@ class EventAlertDialog extends StatefulWidget {
 }
 
 class _EventAlertDialogState extends State<EventAlertDialog> {
-  TextEditingController? controller;
+  TextEditingController controller = TextEditingController();
   bool isIsrael = false;
   Map<String, bool> usComp = <String, bool>{
     "cc": false,
@@ -40,153 +42,182 @@ class _EventAlertDialogState extends State<EventAlertDialog> {
   @override
   Widget build(final BuildContext context) => AlertDialog(
         title: const Text("Choose Event"),
-        actions: <Widget>[
-          TextField(
-            controller: controller,
-            decoration: const InputDecoration(hintText: "Year"),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(
-                8.0,
-              ),
-              child: PitToggle(
-                isSelected: <bool>[isIsrael],
-                onPressed: (final int i) {
-                  setState(() {
-                    isIsrael = !isIsrael;
-                    isIsrael
-                        ? usComp.updateAll(
-                            (final String key, bool value) => value = false,
-                          )
-                        : israelComp.updateAll(
-                            (final String key, bool value) => value = false,
-                          );
-                  });
-                },
-                titles: const <String>[" Is Israel? "],
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(hintText: "Year"),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(
+                  8.0,
+                ),
+                child: PitToggle(
+                  isSelected: <bool>[isIsrael],
+                  onPressed: (final int i) {
+                    setState(() {
+                      isIsrael = !isIsrael;
+                      isIsrael
+                          ? usComp.updateAll(
+                              (final String key, bool value) => value = false,
+                            )
+                          : israelComp.updateAll(
+                              (final String key, bool value) => value = false,
+                            );
+                      isIsrael
+                          ? whatSubDivision.updateAll(
+                              (final String key, bool value) => value = false,
+                            )
+                          : districtNum.updateAll(
+                              (final String key, bool value) => value = false,
+                            );
+                    });
+                  },
+                  titles: const <String>[" Is Israel? "],
+                ),
               ),
             ),
-          ),
-          isIsrael
-              ? Center(
-                  child: Expanded(
+            isIsrael
+                ? Center(
+                    child: Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: PitToggle(
+                          onPressed: (final int index) {
+                            setState(() {
+                              !israelComp["de"]!
+                                  ? districtNum.updateAll(
+                                      (final String key, bool value) =>
+                                          value = false,
+                                    )
+                                  : null;
+                              israelComp.updateAll(
+                                (final String key, bool value) => value = false,
+                              );
+                              final List<String> keys =
+                                  israelComp.keys.toList();
+                              final String key = keys[index];
+                              israelComp[key] = !israelComp[key]!;
+                            });
+                          },
+                          isSelected: israelComp.values.toList(),
+                          titles: const <String>[
+                            " Dcmp? ",
+                            " Ios? ",
+                            " District? ",
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                : Center(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: PitToggle(
                         onPressed: (final int index) {
                           setState(() {
-                            israelComp.updateAll(
+                            !usComp["worlds"]!
+                                ? whatSubDivision.updateAll(
+                                    (final String key, bool value) =>
+                                        value = false,
+                                  )
+                                : null;
+                            usComp.updateAll(
                               (final String key, bool value) => value = false,
                             );
-                            final List<String> keys = israelComp.keys.toList();
+                            final List<String> keys = usComp.keys.toList();
                             final String key = keys[index];
-                            israelComp[key] = !israelComp[key]!;
+                            usComp[key] = !usComp[key]!;
                           });
                         },
-                        isSelected: israelComp.values.toList(),
+                        isSelected: usComp.values.toList(),
                         titles: const <String>[
-                          " Dcmp? ",
-                          " Ios? ",
-                          " District? ",
+                          " Chezy Champs ",
+                          " IRI",
+                          " Worlds ",
                         ],
                       ),
                     ),
                   ),
-                )
-              : Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: PitToggle(
-                      onPressed: (final int index) {
-                        setState(() {
-                          usComp.updateAll(
-                            (final String key, bool value) => value = false,
-                          );
-                          final List<String> keys = usComp.keys.toList();
-                          final String key = keys[index];
-                          usComp[key] = !usComp[key]!;
-                        });
-                      },
-                      isSelected: usComp.values.toList(),
-                      titles: const <String>[
-                        " Chezy Champs ",
-                        " IRI",
-                        " Worlds ",
-                      ],
+            usComp["worlds"]! && !isIsrael
+                ? Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PitToggle(
+                        onPressed: (final int index) {
+                          setState(() {
+                            whatSubDivision.updateAll(
+                              (final String key, bool value) => value = false,
+                            );
+                            final List<String> keys =
+                                whatSubDivision.keys.toList();
+                            final String key = keys[index];
+                            whatSubDivision[key] = !whatSubDivision[key]!;
+                          });
+                        },
+                        isSelected: whatSubDivision.values.toList(),
+                        titles: const <String>[
+                          " Archimedes Division ",
+                          " Curie Division ",
+                          " Daly Division ",
+                          " Galileo Division ",
+                          " Hopper Division ",
+                          " Johnson Division ",
+                          " Milstein Division ",
+                          " Newton Division ",
+                          " Einstein Field ",
+                        ],
+                      ),
                     ),
-                  ),
-                ),
-          usComp["worlds"]! && !isIsrael
-              ? Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: PitToggle(
-                      onPressed: (final int index) {
-                        setState(() {
-                          whatSubDivision.updateAll(
-                            (final String key, bool value) => value = false,
-                          );
-                          final List<String> keys =
-                              whatSubDivision.keys.toList();
-                          final String key = keys[index];
-                          whatSubDivision[key] = !whatSubDivision[key]!;
-                        });
-                      },
-                      isSelected: whatSubDivision.values.toList(),
-                      titles: const <String>[
-                        " Archimedes Division ",
-                        " Curie Division ",
-                        " Daly Division ",
-                        " Galileo Division ",
-                        " Hopper Division ",
-                        " Johnson Division ",
-                        " Milstein Division ",
-                        " Newton Division ",
-                        " Einstein Field ",
-                      ],
+                  )
+                : Container(),
+            israelComp.values.last && isIsrael
+                ? Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PitToggle(
+                        onPressed: (final int index) {
+                          setState(() {
+                            districtNum.updateAll(
+                              (final String key, bool value) => value = false,
+                            );
+                            final List<String> keys = districtNum.keys.toList();
+                            final String key = keys[index];
+                            districtNum[key] = !districtNum[key]!;
+                          });
+                        },
+                        isSelected: districtNum.values.toList(),
+                        titles: const <String>[
+                          " Dis 1 ",
+                          " Dis 2 ",
+                          " Dis 3 ",
+                          " Dis 4 ",
+                        ],
+                      ),
                     ),
-                  ),
-                )
-              : Container(),
-          israelComp.values.last && isIsrael
-              ? Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: PitToggle(
-                      onPressed: (final int index) {
-                        setState(() {
-                          districtNum.updateAll(
-                            (final String key, bool value) => value = false,
-                          );
-                          final List<String> keys = districtNum.keys.toList();
-                          final String key = keys[index];
-                          districtNum[key] = !districtNum[key]!;
-                        });
-                      },
-                      isSelected: districtNum.values.toList(),
-                      titles: const <String>[
-                        " Dis 1 ",
-                        " Dis 2 ",
-                        " Dis 3 ",
-                        " Dis 4 ",
-                      ],
-                    ),
-                  ),
-                )
-              : Container(),
+                  )
+                : Container(),
+          ],
+        ),
+        actions: <Widget>[
           TextButton(
-            onPressed: () => Navigator.pop(
-              context,
-              parseEvent(
-                isIsrael,
-                israelComp,
-                districtNum,
-                whatSubDivision,
-                controller!.text,
-                usComp,
-              ),
-            ),
+            onPressed: () {
+              getWebsiteData(
+                parseEvent(
+                  isIsrael,
+                  israelComp,
+                  districtNum,
+                  whatSubDivision,
+                  controller.text,
+                  usComp,
+                ),
+              ).then(
+                (final List<BlueAllianceMatchData>? value) =>
+                    Navigator.pop(context, value),
+              );
+            },
             child: const Text("Submit Button"),
           ),
         ],
@@ -201,19 +232,35 @@ String parseEvent(
   final String year,
   final Map<String, bool> usComp,
 ) {
-  final String isrealCompKey = israelComp.entries
-      .firstWhere((final MapEntry<String, bool> entry) => entry.value == true)
-      .key;
-  final String districtNumKey = districtNum.entries
-      .firstWhere((final MapEntry<String, bool> entry) => entry.value == true)
-      .key;
-  final String whatSubDivisionKey = whatSubDivision.entries
-      .firstWhere((final MapEntry<String, bool> entry) => entry.value == true)
-      .key;
+  final String isrealCompKey = israelComp.values.contains(true)
+      ? israelComp.entries
+          .firstWhere(
+            (final MapEntry<String, bool> entry) => entry.value == true,
+          )
+          .key
+      : "";
+  final String districtNumKey = districtNum.values.contains(true)
+      ? districtNum.entries
+          .firstWhere(
+            (final MapEntry<String, bool> entry) => entry.value == true,
+          )
+          .key
+      : "";
+  final String whatSubDivisionKey = whatSubDivision.values.contains(true)
+      ? whatSubDivision.entries
+          .firstWhere(
+            (final MapEntry<String, bool> entry) => entry.value == true,
+          )
+          .key
+      : "";
   final bool worlds = usComp["worlds"]!;
   usComp.remove("worlds");
-  final String usCompKey = usComp.entries
-      .firstWhere((final MapEntry<String, bool> entry) => entry.value == true)
-      .key;
-  return "$year/${isIsreal ? "is" : ""}/$isrealCompKey$districtNumKey${worlds ? whatSubDivisionKey : ""}$usCompKey";
+  final String usCompKey = usComp.values.contains(true)
+      ? usComp.entries
+          .firstWhere(
+            (final MapEntry<String, bool> entry) => entry.value == true,
+          )
+          .key
+      : "";
+  return "$year${isIsreal ? "is" : ""}$isrealCompKey$districtNumKey${worlds ? whatSubDivisionKey : ""}$usCompKey";
 }
