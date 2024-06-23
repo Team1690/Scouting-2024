925dfe54cc19c19d1bf118ef068210b35e70fd9d
BOSSkabab <amit.askof@gmail.com>
2024-06-23 22:09:15 +0300

    implemented the widgets

--- lib/views/pc/compare blue alliance/blue_alliance_compare_screen.dart
+++ lib/views/pc/compare blue alliance/blue_alliance_compare_screen.dart
@@ -6,14 +6,12 @@ import "package:scouting_frontend/net/hasura_helper.dart";
 import "package:scouting_frontend/views/common/card.dart";
 import "package:scouting_frontend/views/common/dashboard_scaffold.dart";
 import "package:scouting_frontend/views/pc/compare%20blue%20alliance/blue_alliance_match_data.dart";
-import "package:scouting_frontend/views/pc/compare%20blue%20alliance/fetch_match_results.dart";
 import "package:scouting_frontend/views/pc/compare%20blue%20alliance/event_alert_dialog.dart";
 
 class BlueAllianceCompareScreen extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
-    Future<List<BlueAllianceMatchData>?> blueAllianceMatches;
-
+    late List<BlueAllianceMatchData>? blueAllianceMatches;
     return DashboardScaffold(
       body: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -21,13 +19,13 @@ class BlueAllianceCompareScreen extends StatelessWidget {
           title: "Blue Alliance Compare ",
           titleWidgets: <Widget>[
             IconButton(
-              onPressed: () {
-                final Future<String?> event = showDialog<String>(
+              onPressed: () async {
+                blueAllianceMatches =
+                    await showDialog<List<BlueAllianceMatchData>?>(
                   context: context,
                   builder: (final BuildContext innerContext) =>
                       EventAlertDialog(),
                 );
-                blueAllianceMatches = getWebsiteData(event as String);
               },
               icon: const Icon(Icons.query_stats),
             ),
